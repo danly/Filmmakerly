9547840fe23a149167b180315840388248ab1ece
“Stephen <stephen@stepheneyer.comssh-keygen -t rsa -C stephen@stepheneyer.com>
2015-05-22 18:29:08 -0700

    Basic signup added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var db = require("./models");
 
 var app = express();
 
@@ -11,6 +12,23 @@ var views = path.join(__dirname, "views");
 app.get("/", function (req, res) {
 	var homePath = path.join(views, "home.html");
 	res.sendFile(homePath);
+});
+
+app.get("/signup", function (req, res){
+	var signupPath = path.join(views, "signup.html");
+	res.sendFile(signupPath);
+});
+
+app.post("/users", function (req, res){
+	var newUser = req.body.user;
+	db.User.
+	createSecure(newUser, function (err, user){
+		if (user) {
+			res.send(user);
+		} else {
+			res.redirect("/signup");
+		}
+	});
 });
 
 app.listen(3000, function () {
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
 
 var userSchema = new mongoose.Schema({
 					email: {
@@ -43,6 +44,29 @@ var userSchema = new mongoose.Schema({
 						type: String
 					}
 });
+
+var confirm = function (pswrd, pswrdCon) {
+	return pswrd === pswrdCon;
+};
+
+userSchema.statics.createSecure = function (params, cb) {
+	var isConfirmed;
+
+	isConfirmed = confirm(params.password, params.password_confirmation);
+
+	if (!isConfirmed) {
+		return cb("Passwords should match", null);
+	}
+
+	var that = this;
+
+	bcrypt.hash(params.password, 12, function (err, hash){
+		params.passwordDigest = hash;
+		that.create(params, cb);
+	});
+};
+
+// need to also do frontend validations
 
 var User = mongoose.model("User", userSchema);
 
